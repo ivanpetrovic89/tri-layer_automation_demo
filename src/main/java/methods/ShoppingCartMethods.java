e9374987b12ca3995bd6d8091bfe3bfa2c3a980f
ivanpetrovic89 <ivanpetrovic89@gmail.com>
2023-01-11 08:48:19 +0100

    Finished documentation for ShoppingCartMethods and ShoppingCartPage.

--- src/main/java/methods/ShoppingCartMethods.java
+++ src/main/java/methods/ShoppingCartMethods.java
@@ -1,23 +1,52 @@
 package methods;
 
 import org.openqa.selenium.WebDriver;
+import pages.BasePage;
 import pages.ShopPage;
 import pages.ShoppingCartPage;
 
+/**
+ * ShoppingCartMethods is a class representing the methods that perform various actions related to the Shopping Cart
+ * in the web application under test.
+ * <p>
+ * It extends {@link BasePage}.
+ */
+
 public class ShoppingCartMethods extends ShoppingCartPage {
+    /**
+     * Constructor that instantiate a new object of ShoppingCartMethods class.
+     *
+     * @param driver WebDriver instance that will be used to interact with the page.
+     */
     public ShoppingCartMethods(WebDriver driver) {
         super(driver);
     }
 
+    /**
+     * Removes the items from the cart.
+     */
     public void removeFromCart(){
         clickElement(ShoppingCartPage.removeButtonBy);
     }
 
+
+    /**
+     * Checks if item is removed from cart.
+     *
+     * @param expectedText expected text of cart icon element.
+     */
     public void isItemRemovedFromCartCheck(String expectedText){
         ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
         assertStringsEqual(shoppingCartPage.readCartNumber(), expectedText);
     }
 
+
+    /**
+     * Checks if the item with minimum and maximum price are added to the cart.
+     *
+     * @param expectedMinPrice min price of the item
+     * @param expectedMaxPrice max price of the item
+     */
     public void isMinMaxPriceItemAddedCheck(String expectedMinPrice, String expectedMaxPrice){
         ShopPage shopPage = new ShopPage(driver);
         assertStringsEqual(shopPage.minPrice(), expectedMinPrice);
--- src/main/java/pages/ShoppingCartPage.java
+++ src/main/java/pages/ShoppingCartPage.java
@@ -4,24 +4,68 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utilities.PropertyManager;
 
+/**
+ * ShoppingCartPage is a class representing the shopping cart page in the site under test.
+ * <p>
+ * It contains all the elements and methods that interact with the shopping cart page and also implements
+ * all the functionality that a user can perform on this page.
+ * It extends {@link BasePage}.
+ */
+
 public class ShoppingCartPage extends BasePage{
+    /**
+     * Constructor that instantiate a new object of ShoppingCartPage class.
+     *
+     * @param driver WebDriver instance that will be used to interact with the page.
+     */
     public ShoppingCartPage(WebDriver driver) {
         super(driver);
     }
+
+    /**
+     * By element to locate the remove button.
+     */
     public static By removeButtonBy = By.xpath("//button[text()='Remove']");
+
+    /**
+     * By element to locate the navigate to cart button.
+     */
     public static By navigateToCartButtonBy = By.className("shopping_cart_link");
+
+    /**
+     * String variable that stores expected minimum price of the item from {@link PropertyManager}.
+     */
     public String expectedMinPrice = PropertyManager.getExpectedMinPrice();
+
+    /**
+     * String variable that stores expected maximum price of the item from {@link PropertyManager}.
+     */
     public String expectedMaxPrice = PropertyManager.getExpectedMaxPrice();
+
+    /**
+     * By element to locate the checkout button.
+     */
     public By checkoutButtonBy = By.id("checkout");
 
+    /**
+     * Check whether the remove button is displayed.
+     */
     public void isItemAddedToCartCheck(){
         isElementDisplayed(ShoppingCartPage.removeButtonBy);
     }
 
+    /**
+     * Returns the text of the cart icon element.
+     *
+     * @return The text of the cart icon element.
+     */
     public String readCartNumber(){
         return readText(navigateToCartButtonBy);
     }
 
+    /**
+     * Performs click action on checkout button.
+     */
     public void goToCheckout(){
         clickElement(checkoutButtonBy);
     }
